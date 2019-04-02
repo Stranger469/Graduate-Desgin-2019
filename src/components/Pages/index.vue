238a42cb0533ec5065ab456245cf27ab0be1a19e
AlexSong <Stranger469@icloud.com>
2019-04-02 22:45:29 +0800

    爆肝

---
+++ src/components/Pages/index.vue
@@ -0,0 +1,138 @@
+<template>
+    <div class="site-page">
+       <div class="msg">
+         共<span>{{totalPage}}</span>页/<span>{{total}}</span>条数据
+       </div>
+       <div class="content">
+         <span class="page-item" @click="toPage(curPage-1)"> << </span>
+         <template  v-if="startPage>=showSizeMax">
+            <span class="page-item" @click="toPage(1)">1</span>
+            <span class="page-item">...</span>
+         </template>
+         <template  v-for="(index,key) in showSize">
+           <span
+            class="page-item"
+            :class="{cur:(key + startPage === curPage)}"
+            @click="toPage(key + startPage)">{{ key + startPage}}</span>
+         </template>
+         <template  v-if="(startPage + showSize - 1) !== totalPage">
+           <span class="page-item">...</span>
+           <span class="page-item" @click="toPage(totalPage)">{{totalPage}}</span>
+         </template>
+         <span class="page-item"@click="toPage(curPage+1)"> >> </span>
+       </div>
+       <div class="jump">
+          <span @click="toPage(inValue)">跳至</span>
+          <input class="page-item" v-model='inValue'>
+          页
+       </div>
+    </div>
+</template>
+<style lang="less">
+  .site-page{
+    text-align: left;
+    display:inline-block;
+    font-size:12px;
+    user-select: none;
+    .page-item{
+      width: 35px;
+      height: 25px;
+      line-height: 25px;
+      box-sizing: border-box;
+      text-align: center;
+      border: 1px solid rgba(215, 215, 215, 1);
+      font-size: 13px;
+      border-radius: 3px;
+      cursor: pointer;
+      display: inline-block;
+      vertical-align: middle;
+      &.cur{
+        background: #0064D3;
+        color:#fff;
+        border-color:#0064D3;
+       }
+    }
+    .msg{
+      vertical-align: middle;
+      display: inline-block;
+      margin-right:10px;
+      span{
+        color: #FF3C2C;
+      }
+    }
+    .content{
+      display: inline-block;
+      vertical-align: middle;
+      font-size: 0;
+      .page-item {
+        margin-right: 5px;
+      }
+    }
+    .jump{
+      vertical-align: middle;
+      display: inline-block;
+      margin-left:10px;
+      color:#999;
+      span{
+        margin-right:8px;
+        cursor: pointer;
+        color: #0064D3;
+      }
+      input{
+        margin-right:8px;
+      }
+    }
+  }
+
+
+</style>
+<script>
+export default{
+  name: 'page',
+  props: [
+    'total',
+    'curPage',
+    'pageSize',
+  ],
+  data() {
+    return {
+      // pageSize:10,
+      showSizeMax: 6,
+      inValue: '',
+    };
+  },
+  computed: {
+    totalPage() {
+      const totalPage = this.total / this.pageSize;
+      return totalPage == 0 ? 0 : Math.ceil(totalPage);
+    },
+    startPage() {
+      const rightSize = this.showSizeMax - 1;
+      const num = Math.floor(this.curPage / rightSize);
+      const group = this.curPage % rightSize == 0 ? num - 1 : num;
+      const start = group * rightSize + 1;
+      // console.log(group);
+      return start == 0 ? 1 : start;
+      // 1 8 15 22 29
+    },
+    showSize() {
+      const max = this.startPage - 1 + this.showSizeMax;
+      const min = max - this.totalPage;
+      return min > 0 ? this.showSizeMax - min : this.showSizeMax;
+    },
+  },
+  methods: {
+    toPage(page) {
+      if (page < 1) {
+        page = 1;
+      } else if (page > this.totalPage) {
+        page = this.totalPage;
+      }
+      this.$emit('setCurPage', page);
+      this.inValue = '';
+      //        this.curPage = page;
+    },
+  },
+};
+
+</script>
--- src/page/corp/admin/emplMgn.vue
+++ src/page/corp/admin/emplMgn.vue
@@ -1,23 +1,146 @@
 <template>
-  <div>
-    员工管理
+  <div class="emplMgn">
+    <div class="card-container" v-for="(empl, index) of empls" :key="index">
+      <div class="card">
+        <div class="row">
+          <div class="name">{{ empl[0].name }}<span class="close">x</span></div>
+        </div>
+        <div class="row">
+          <span>电话:</span> {{ empl[0].tel }}
+          <span><font size="1">部门: </font>{{ empl[0].dept }}</span>
+        </div>
+        <div class="row">
+          <span>邮箱:</span> {{ empl[0].email }}
+          <span><div class="btn primary">修改</div></span>
+        </div>
+      </div>
+      <div class="card" style="margin: 0 10px 0 10px">
+        <div class="row">
+          <div class="name">{{ empl[1].name }}<span class="close">x</span></div>
+        </div>
+        <div class="row">
+          <span>电话:</span> {{ empl[1].tel }}
+          <span><font size="1">部门: </font>{{ empl[1].dept }}</span>
+        </div>
+        <div class="row">
+          <span>邮箱:</span> {{ empl[1].email }}
+          <span><div class="btn primary">修改</div></span>
+        </div>
+      </div>
+      <div class="card">
+        <div class="row">
+          <div class="name">{{ empl[2].name }}<span class="close">x</span>
+        </div>
+        </div>
+        <div class="row">
+          <span>电话:</span> {{ empl[2].tel }}
+          <span><font size="1">部门: </font>{{ empl[2].dept }}</span>
+        </div>
+        <div class="row">
+          <span>邮箱:</span> {{ empl[2].email }}
+          <span><div class="btn primary">修改</div></span>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <Page @setCurPage="setCurPage" :curPage="curPage" :total="total" :pageSize="pageSize"></Page>
+    </footer>
   </div>
 </template>
 <script>
 import { corpMixin } from '@/mixins/NavigationGuards';
+import Page from '@/components/Pages/index';
 
 export default {
   mixins: [corpMixin],
   name: 'EmplMgn',
   data() {
     return {
-
+      empls: [
+        [
+          { name: '李博今', tel: '17612082048', dept: '傻屌总部 - 傻屌分部', email: '1034356409@qq.com' },
+          { name: '李博今', tel: '17612082048', dept: '傻屌总部 - 傻屌分部', email: '1034356409@qq.com' },
+          { name: '李博今', tel: '17612082048', dept: '傻屌总部 - 傻屌分部', email: '1034356409@qq.com' },
+        ],
+        [
+          { name: '李博今', tel: '17612082048', dept: '傻屌总部 - 傻屌分部', email: '1034356409@qq.com' },
+          { name: '李博今', tel: '17612082048', dept: '傻屌总部 - 傻屌分部', email: '1034356409@qq.com' },
+          { name: '李博今', tel: '17612082048', dept: '傻屌总部 - 傻屌分部', email: '1034356409@qq.com' },
+        ],
+        [
+          { name: '李博今', tel: '17612082048', dept: '傻屌总部 - 傻屌分部', email: '1034356409@qq.com' },
+          { name: '李博今', tel: '17612082048', dept: '傻屌总部 - 傻屌分部', email: '1034356409@qq.com' },
+          { name: '李博今', tel: '17612082048', dept: '傻屌总部 - 傻屌分部', email: '1034356409@qq.com' },
+        ],
+      ],
+      curPage: 1,
+      total: 1000,
+      pageSize: 10,
     };
+  },
+  components: {
+    Page,
+  },
+  methods: {
+    setCurPage(page) {
+      this.curPage = page;
+    },
   },
 };
 </script>
 <style lang="less">
 @import url(../../../../static/style/site.comm.less);
 @import url(../../../../static/style/site.vars.less);
-
+.emplMgn {
+  padding-top: 40px;
+  .card-container {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    margin-bottom: 10px;
+    .card {
+      position: relative;
+      background: rgba(103, 174, 255, 0.3);
+      width: 100%;
+      height: 150px;
+      box-sizing: border-box;
+      padding: 15px;
+      .row {
+        user-select: none;
+        color: @site-main;
+        box-sizing: border-box;
+        padding: 0px;
+        height: 45px;
+        .close {
+          cursor: pointer;
+          float: right;
+          font-size: 12px;
+        }
+        .name {
+          color: rgb(0, 64, 138);
+          font-size: 24px;
+        }
+        .tel {
+          color: #fff;
+        }
+        & > span:nth-child(1) {
+          font-size: 10px;
+        }
+        & > span:nth-child(2) {
+          float: right;
+        }
+      }
+    }
+  }
+  & > footer {
+    margin-top: 30px;
+    padding: 20px 0 20px 0;
+    border: 1px rgba(0, 97, 207, 0.3) solid;
+    width: 100%;
+    background: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
